cb38ec2a475582ecef50b70aa49bc3fb30669fcf
drong24 <daisyrong24@gmail.com>
2024-08-27 02:17:20 -0700

    moved map initiation from index.html to index.js

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 
 
-
 async function init() {
+
+  const { Map } = await google.maps.importLibrary("maps");
+  const mapDiv = document.getElementById("map");
+  const map = new google.maps.Map(mapDiv, {
+    zoom: 15,
+    center: new google.maps.LatLng(37.4419, -122.1419),
+  });
+
+  google.maps.event.addListener(map, 'click', function(e) {
+    console.log(e.placeId);
+    e.stop();
+  });
+
+  /*
     await customElements.whenDefined('gmp-map');
   
-    const map = document.querySelector('gmp-map');
+    const map = new google.maps.Map(
+      document.getElementById("map"), {
+        center: new google.maps.LatLng(37.4419, -122.1419),
+        zoom: 13,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      });
+    map.fitBounts
+    //const map = document.querySelector('gmp-map');
     const marker = document.querySelector('gmp-advanced-marker');
     const placePicker = document.querySelector('gmpx-place-picker');
     const infowindow = new google.maps.InfoWindow();
@@ -12,7 +32,14 @@ async function init() {
     map.innerMap.setOptions({
       mapTypeControl: false
     });
-  
+
+    google.maps.event.addListener(placePicker, "click", function(){
+      console.log("Map clicked");
+  });
+    placePicker.addEventListener('click', (e) => {
+      console.log(e);
+    });
+    /*
     placePicker.addEventListener('gmpx-placechange', () => {
       const place = placePicker.value;
   
@@ -35,10 +62,12 @@ async function init() {
       marker.position = place.location;
       infowindow.setContent(
         `<strong>${place.displayName}</strong><br>
-         <span>${place.formattedAddress}</span>
+         <span>${place.formattedAddress}</span> <br>
+         <span>${place.rating}</span>
       `);
       infowindow.open(map.innerMap, marker);
     });
+    */
   }
   
 document.addEventListener('DOMContentLoaded', init);
@@ -46,60 +75,3 @@ document.addEventListener('DOMContentLoaded', init);
 
   // Nearby Search
 
-  let map;
-  let center;
-  
-  async function initMap() {
-    const { Map } = await google.maps.importLibrary("maps");
-  
-    center = { lat: 37.4161493, lng: -122.0812166 };
-    map = new Map(document.getElementById("map"), {
-      center: center,
-      zoom: 11,
-      mapId: "DEMO_MAP_ID",
-    });
-    findPlaces();
-  }
-  
-  async function findPlaces() {
-    const { Place } = await google.maps.importLibrary("places");
-    const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-    const request = {
-      textQuery: "Tacos in Mountain View",
-      fields: ["displayName", "location", "businessStatus"],
-      includedType: "restaurant",
-      locationBias: { lat: 37.4161493, lng: -122.0812166 },
-      isOpenNow: true,
-      language: "en-US",
-      maxResultCount: 8,
-      minRating: 3.2,
-      region: "us",
-      useStrictTypeFiltering: false,
-    };
-    //@ts-ignore
-    const { places } = await Place.searchByText(request);
-  
-    if (places.length) {
-      console.log(places);
-  
-      const { LatLngBounds } = await google.maps.importLibrary("core");
-      const bounds = new LatLngBounds();
-  
-      // Loop through and get all the results.
-      places.forEach((place) => {
-        const markerView = new AdvancedMarkerElement({
-          map,
-          position: place.location,
-          title: place.displayName,
-        });
-  
-        bounds.extend(place.location);
-        console.log(place);
-      });
-      map.fitBounds(bounds);
-    } else {
-      console.log("No results");
-    }
-  }
-  
-  initMap();
